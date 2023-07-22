45237511ffe9628d73ca01d6752ce37facdbebe9
axlschtn <ext.aschouten@cma-cgm.com>
2023-07-22 23:48:15 +0200

    Test Python project

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+import pytest
+
+def test_say_hello_to_axel():
+	result = "say hello, axel!"
+	assert result == "say hello, axel!"
+
+def test_sum_equal_to_4():
+	sum = 2 + 2
+	assert sum == 4
+
+def test_is_active_user():
+	is_active = true
+	assert is_active == true
